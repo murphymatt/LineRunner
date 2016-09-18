2e4b770306741a999acde98ef79af10981bb4e91
murphymatt <murphyma@grinnell.edu>
2016-09-18 00:21:08 -0400

    created listener object

--- listener.py
+++ listener.py
@@ -1,33 +1,33 @@
 import speech_recognition as sr
 
-def listenToSpeech():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
+class Listener:
+
+    def __init__(self):
+        self.r = sr.Recognizer()
+
+    def listen_to_speech(self):
         print ("Recording... ")
-        audio = r.listen(source)
+        with sr.Microphone() as source:
+            audio = self.r.listen(source)
 
-    # Speech recognition using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        print("You said: " + r.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        # Speech recognition using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            print("You said: " + self.r.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}"\
+                  .format(e))
 
-    # outputs parsed audio
-    return r.recognize_google(audio)
+        # outputs parsed audio
+        return self.r.recognize_google(audio)
 
-def checkSpeech(spoken, actual):
-    # fix parsed speech and actual text (only alpha)
-#    spoken = spoken.translate(None, ',.;:\"\'')
-#    actual = actual.translate(None, ',.;:\"\'')
-    if spoken == actual:
-        return True
-    else:
-        return False
-
-# simple testing"
-s = listenToSpeech()
-print checkSpeech(s, "box")
-print checkSpeech(s, "hello")
+    def check_speech(self, spoken, actual):
+        # fix parsed speech and actual text (only alpha)
+    #    spoken = spoken.translate(None, ',.;:\"\'')
+    #    actual = actual.translate(None, ',.;:\"\'')
+        if spoken == actual:
+            return True
+        else:
+            return False
