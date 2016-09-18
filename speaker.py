f0b92eebe9249527954a64ab96063393b377a088
murphymatt <murphyma@grinnell.edu>
2016-09-17 23:49:33 -0400

    Changed to object

--- speaker.py
+++ speaker.py
@@ -1,6 +1,10 @@
 import pyttsx as tts
 
-def sayLine(line):
-    engine = tts.init()
-    engine.say(line)
-    engine.runAndWait()
+class Speaker:
+
+    def __init__(self):
+        self.engine = tts.init()
+        
+    def say_line(self, line):
+        self.engine.say(line)
+        self.engine.runAndWait()
