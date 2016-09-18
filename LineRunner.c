08785743a3fad433d4f5b805435e0c42a630e632
Alex Mitchell <alex@alexthemitchell.com>
2016-09-17 21:23:13 -0400

    Finish script parsing

--- LineRunner.c
+++ LineRunner.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-  
+typedef struct LinePair {
+  char *character;
+  char *line;
+} LinePair;
+
+static const char delimiters[] = ":";
+
+int readLine(FILE *file, LinePair *out);
+
+int main(int argc, char *argv[]) {
+
+  FILE *script;  
+  switch (argc) {
+    case 1: // No arguments, default to file named SCRIPT
+      script = fopen("SCRIPT", "r");
+      break;
+  }
+
+  LinePair line; 
+  while (readLine(script, &line)){
+    printf("%s => %s\n", line.character, line.line); 
+  }
+  return fclose(script);
+}
+
+int readLine(FILE *file, LinePair *out) {
+  char read[256];
+  char *running;
+  if (fgets(read, sizeof read, file) != NULL) {
+    running = strdup(read);
+    out->character = strsep(&running, delimiters);
+    out->line = strsep(&running, delimiters);    
+    return 1;
+  }
   return 0;
 }
